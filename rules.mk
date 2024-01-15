9dc90d3c3f2fb70304a7fa2da78813d2df874a6c
grytole <grytole@gmail.com>
2024-01-15 17:11:16 +0200

    cleanup make files

--- rules.mk
+++ rules.mk
@@ -13,43 +13,43 @@
 #    (ie, the full set of cpu arch flags, _none_ are defined in this file)
 #
 ### OPTIONAL ###
-# INCLUDES - fully formed -I paths, if you want extra, eg -I../shared
+# INCLUDES - include paths, if you want extra, eg: INCLUDES = shared libs/some/inc
 # BUILD_DIR - defaults to bin, should set this if you are building multiarch
 # OPT - full -O flag, defaults to -Os
 # CSTD - defaults -std=c99
 # CXXSTD - no default.
 
+include $(OPENCM3_DIR)/mk/genlink-config.mk
+
 BUILD_DIR ?= bin
 OPT ?= -Os
 CSTD ?= -std=c99
 
-# Be silent per default, but 'make V=1' will show all compiler calls.
-# If you're insane, V=99 will print out all sorts of things.
-V?=0
+V ?= 0
 ifeq ($(V),0)
-Q	:= @
-NULL	:= 2>/dev/null
+ Q := @
+ NULL	:= 2>/dev/null
 endif
 
-# Tool paths.
-PREFIX	?= arm-none-eabi-
-CC	= $(PREFIX)gcc
-LD	= $(PREFIX)gcc
-OBJCOPY	= $(PREFIX)objcopy
-OBJDUMP	= $(PREFIX)objdump
+PREFIX ?= arm-none-eabi-
+CC = $(PREFIX)gcc
+LD = $(PREFIX)gcc
+OBJCOPY = $(PREFIX)objcopy
+OBJDUMP = $(PREFIX)objdump
+SIZE = $(PREFIX)size
 STM32FLASH = $(shell which stm32flash)
 
 OPENCM3_INC = $(OPENCM3_DIR)/include
 
-# Inclusion of library header files
-INCLUDES += $(patsubst %,-I%, . $(OPENCM3_INC) )
+TGT_IFLAGS += $(patsubst %,-I%,$(INCLUDES))
+TGT_IFLAGS += $(patsubst %,-I%, . $(OPENCM3_INC) )
 
 OBJS = $(CFILES:%.c=$(BUILD_DIR)/%.o)
 GENERATED_BINS = $(PROJECT).elf $(PROJECT).bin $(PROJECT).map $(PROJECT).list $(PROJECT).lss
 
 TGT_CPPFLAGS += -MD
-TGT_CPPFLAGS += -Wall -Wundef $(INCLUDES)
-TGT_CPPFLAGS += $(INCLUDES) $(OPENCM3_DEFS)
+TGT_CPPFLAGS += -Wall -Wundef
+TGT_CPPFLAGS += $(TGT_IFLAGS) $(OPENCM3_DEFS)
 
 TGT_CFLAGS += $(OPT) $(CSTD) -ggdb3
 TGT_CFLAGS += $(ARCH_FLAGS)
@@ -69,70 +69,72 @@ TGT_LDFLAGS += $(ARCH_FLAGS)
 TGT_LDFLAGS += -specs=nano.specs
 TGT_LDFLAGS += -Wl,--gc-sections
 ifeq ($(V),99)
-TGT_LDFLAGS += -Wl,--print-gc-sections
+ TGT_LDFLAGS += -Wl,--print-gc-sections
 endif
 
 # Linker script generator fills this in for us.
 ifeq (,$(DEVICE))
-LDLIBS += -l$(OPENCM3_LIB)
+ LDLIBS += -l$(OPENCM3_LIB)
 endif
 LDLIBS += -Wl,--start-group -lc -lgcc -lnosys -Wl,--end-group
 
-# Burn in legacy hell fortran modula pascal yacc idontevenwat
 .SUFFIXES:
 .SUFFIXES: .c .h .o .cxx .elf .bin .list .lss
 
-# Bad make, never *ever* try to get a file out of source control by yourself.
 %: %,v
 %: RCS/%,v
 %: RCS/%
 
-all: $(PROJECT).elf $(PROJECT).bin
+all: $(PROJECT).elf $(PROJECT).bin stat
 flash: $(PROJECT).flash
 
 # error if not using linker script generator
 ifeq (,$(DEVICE))
 $(LDSCRIPT):
 ifeq (,$(wildcard $(LDSCRIPT)))
-    $(error Unable to find specified linker script: $(LDSCRIPT))
+ $(error Unable to find specified linker script: $(LDSCRIPT))
 endif
 else
-# if linker script generator was used, make sure it's cleaned.
-GENERATED_BINS += $(LDSCRIPT)
+ # if linker script generator was used, make sure it's cleaned.
+ GENERATED_BINS += $(LDSCRIPT)
 endif
 
-# Need a special rule to have a bin dir
 $(BUILD_DIR)/%.o: %.c
-	@printf "  CC\t$<\n"
+	@printf "  CC      $<\n"
 	@mkdir -p $(dir $@)
 	$(Q)$(CC) $(TGT_CFLAGS) $(CFLAGS) $(TGT_CPPFLAGS) $(CPPFLAGS) -o $@ -c $<
 
 $(BUILD_DIR)/%.o: %.cxx
-	@printf "  CXX\t$<\n"
+	@printf "  CXX     $<\n"
 	@mkdir -p $(dir $@)
 	$(Q)$(CC) $(TGT_CXXFLAGS) $(CXXFLAGS) $(TGT_CPPFLAGS) $(CPPFLAGS) -o $@ -c $<
 
 $(PROJECT).elf: $(OBJS) $(LDSCRIPT) $(LIBDEPS)
-	@printf "  LD\t$@\n"
+	@printf "  LD      $@\n"
 	$(Q)$(LD) $(TGT_LDFLAGS) $(LDFLAGS) $(OBJS) $(LDLIBS) -o $@
 
 %.bin: %.elf
-	@printf "  OBJCOPY\t$@\n"
-	$(Q)$(OBJCOPY) -O binary  $< $@
+	@printf "  OBJCOPY $@\n"
+	$(Q)$(OBJCOPY) -O binary $< $@
+
+stat: $(PROJECT).bin
+	@printf "\n  BINARY STATS:\n"
+	$(Q)$(SIZE) --target=binary $<
 
 %.lss: %.elf
-	$(OBJDUMP) -h -S $< > $@
+	$(Q)$(OBJDUMP) -h -S $< > $@
 
 %.list: %.elf
-	$(OBJDUMP) -S $< > $@
+	$(Q)$(OBJDUMP) -S $< > $@
 
 %.flash: %.bin
-	@printf "  FLASH\t$<\n"
-	$(STM32FLASH) -w $< -v -g 0x0 /dev/$(FLASH_PORT)
+	@printf "  FLASH   $<\n"
+	$(Q)$(STM32FLASH) -w $< -v -g 0x0 /dev/$(FLASH_PORT)
 
 clean:
-	rm -rf $(BUILD_DIR) $(GENERATED_BINS)
+	$(Q)rm -rf $(BUILD_DIR) $(GENERATED_BINS)
 
 .PHONY: all clean flash
 -include $(OBJS:.o=.d)
 
+include $(OPENCM3_DIR)/mk/genlink-rules.mk
